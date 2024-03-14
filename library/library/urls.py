7e386368049b1911dc9432418c10bf1a1f661ba8
Fahim <fahim180khan@gmail.com>
2024-03-14 19:29:41 +0600

    created users and added to models

--- library/library/urls.py
+++ library/library/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/')), #redirecting the homepage to catalog page
+    path('accounts/', include('django.contrib.auth.urls'))
 
 ]
