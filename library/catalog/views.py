e737a05eda0846e867a97153610ffbc857d061f3
Fahim <fahim180khan@gmail.com>
2024-03-14 14:51:23 +0600

    cmplt models

---
+++ library/catalog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, BookInstance, Author, Language, Genre
+
+# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    context = {
+        'num_books':num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available
+    }
+
+    return render(request, 'catalog/index.html', context=context)
